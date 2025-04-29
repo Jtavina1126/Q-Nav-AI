b8bbfbd30869fcd3e578d02e72262f088cc0f83c
Justin Avina <avinajustin5@icloud.com>
2025-04-29 15:50:35 -0700

    Update harmonic_detector.py

--- harmonic_detector.py
+++ harmonic_detector.py
@@ -1,6 +1,7 @@
 def calculate_harmonic_index(data):
     score = 0
 
+    # Normalize HRV
     if data.hrv >= 80:
         score += 0.35
     elif data.hrv >= 60:
@@ -8,21 +9,25 @@ def calculate_harmonic_index(data):
     else:
         score += 0.1
 
+    # Brainwave ratio
     if 1.3 <= data.alpha_theta_ratio <= 2.0:
         score += 0.3
     else:
         score += 0.15
 
+    # Emotional coherence
     if data.emotional_coherence == 'high':
         score += 0.2
     elif data.emotional_coherence == 'moderate':
         score += 0.1
 
+    # Spiritual state
     if data.spiritual_state == 'surrendered':
         score += 0.15
     elif data.spiritual_state == 'aligned':
         score += 0.1
 
+    # Output
     return {
         "harmonic_index": round(score, 3),
         "frequency_signature": round(430 + score * 10, 2),
