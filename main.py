eb56dc8b4778170d003161264c6b6beaed891cf7
Justin Avina <avinajustin5@icloud.com>
2025-04-29 15:52:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI
 from schemas import ResonanceInput, BlipInput
 from resonance_model import predict_resonance
 from blip_engine import simulate_blip
+from harmonic_detector import calculate_harmonic_index
+from schemas import HarmonicInput
+
 
 app = FastAPI()
+
+@app.post("/detect_harmonic_resonance")
+def detect_harmonic(input_data: HarmonicInput):
+    return calculate_harmonic_index(input_data)
 
 @app.get("/")
 def root():
